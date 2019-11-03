3194d383fdfd41100407dc3c0c8b49a58f16a409
xiaotao0110 <2430053431@qq.com>
2019-11-03 17:46:53 +0800

    config

---
+++ microservice-config-client-eureka/src/main/java/com/itmuch/cloud/ConfigClientEurekaApplication.java
@@ -0,0 +1,15 @@
+package com.itmuch.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ConfigClientEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientEurekaApplication.class, args);
+	}
+
+}
